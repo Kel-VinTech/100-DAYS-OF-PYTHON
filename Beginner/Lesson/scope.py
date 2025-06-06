97a3b3b60fd823c2c7b85e770c5171bdebcf9b6f
kel-vintech <kelvinrexxy@gmail.com>
2025-06-06 12:46:44 +0100

    restructure the code

--- Beginner/Lesson/scope.py
+++ Beginner/Lesson/scope.py
@@ -1,15 +1,12 @@
 import random       
 
-
-print("Welcome to the Number Guessing Game")
-print("I'm thinking of a number between 1 and 100")
+ ### comment this out or remove this if you don't want to output the answer to the users
 
 
-answer = random.randint(1,100)
-print(answer)
-
-def guess_calc(attempts):
+# the code below is a function to do the calculations of the game.
+def guess_calc(attempts, answer):
     
+    ### this while loop keeps running provided the number of attempts is greater than zero.
     while attempts > 0:
 
         print(f"you have {(attempts)} attempts remaining to guess the number ")
@@ -27,25 +24,38 @@ def guess_calc(attempts):
                 print("Too High")
             else:
                 print("Too Low")
-        
+
+    #this code breaks out of the while loop to allow users restart the game           
+    restart_game = input("do you want to restart the game? Type 'Y' or 'N': ").lower()
+    if restart_game == 'y':
+        start_game()
+    else:
+        return    
 
 
-game_level = input("Choose a difficulty. Type 'easy' or 'hard':").lower()
-if game_level == "easy":
-    attempts = 10
-    guess_calc(attempts)
+# the code below is a function to get the game started
+def start_game():
 
-elif game_level == "hard":
-    attempts = 5
-    guess_calc(attempts)
-else:
-    print("Invalid difficulty level. Please restart the game.")
+    print("Welcome to the Number Guessing Game")
+    print("I'm thinking of a number between 1 and 100")
+
+
+    answer = random.randint(1,100)
+    print(answer)
+
+    game_level = input("Choose a difficulty. Type 'easy' or 'hard':").lower()
+    if game_level == "easy":
+        attempts = 10
+        guess_calc(attempts,answer)
+
+    elif game_level == "hard":
+        attempts = 5
+        guess_calc(attempts,answer)
+    else:
+        print("Invalid difficulty level. Please restart the game.")
+    
+start_game()
     
 
-
-# """ TODO:1 write the code functionality for thr hard guess game:
-# the user guess should match the random number generated else reduce the number of times to guess
-# print the lower or high if the guess is higher or lower than the random number
-#  """
 
 
