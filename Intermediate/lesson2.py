859f6b0fc70741b85ca4b7ea900e2127dd1c4662
kel-vintech <kelvinrexxy@gmail.com>
2025-06-11 22:58:34 +0100

    complete project

--- Intermediate/lesson2.py
+++ Intermediate/lesson2.py
@@ -40,15 +40,21 @@ import random
 tinny = t.Turtle()
 t.colormode(255)
 
-def random_color():
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b =  random.randint(0,255)
-    random_color = (r,g,b)
-    return random_color
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0,255)
+#     b =  random.randint(0,255)
+#     random_color = (r,g,b)
+#     return random_color
 
-# colors = [ "blue", "red", "green", "black"]
-
+color_list = [
+(236, 158, 48), (153, 63, 64), (199, 192, 119), (35, 88, 160), (182, 212, 242),
+(57, 45, 37), (250, 229, 139), (71, 136, 167), (98, 56, 124), (101, 153, 51),
+(243, 56, 63), (60, 114, 107), (209, 200, 192), (173, 223, 183), (251, 210, 55),
+(187, 75, 154), (174, 105, 49), (137, 132, 53), (109, 71, 166), (43, 53, 86),
+(168, 178, 230), (56, 180, 172), (238, 184, 183), (212, 91, 36), (58, 76, 30),
+(68, 89, 87), (243, 121, 159), (243, 92, 33), (142, 204, 80), (221, 147, 79)
+ ]
 
 # for _ in range(15):
 #     tinny.forward(10)
@@ -82,7 +88,7 @@ def random_color():
 # tinny.speed("fastest")
 # def draw_sprial(size_of_gap):
 #     for _ in range(int(360 / size_of_gap)):
-#         tinny.color(random_color())
+#         tinny.color(random.choice(color_list))
 #         tinny.setheading(tinny.heading() + size_of_gap)
 #         tinny.circle(100)
 
@@ -91,14 +97,26 @@ def random_color():
 
 
 
+tinny.speed("fastest")
+tinny.penup()
+tinny.hideturtle()
+tinny.setheading(225)
+tinny.forward(300)
+tinny.setheading(0)
+number_of_dot = 101
 
 
+for dot_count in range(1,number_of_dot):
+    tinny.dot(20,random.choice(color_list))
+    tinny.forward(50)
 
-
-
-
-
-
+    if dot_count % 10 == 0:
+        tinny.setheading(90)
+        tinny.forward(50)
+        tinny.setheading(180)
+        tinny.forward(500)
+        tinny.setheading(0)
+        
 
 
 
