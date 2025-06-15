26d6f45e7523c4a2d95fbcd750e295201c579d8d
kel-vintech <kelvinrexxy@gmail.com>
2025-06-15 11:12:44 +0100

    refactor code

--- Intermediate/Pong Game/main.py
+++ Intermediate/Pong Game/main.py
@@ -7,31 +7,44 @@ screen = Screen()
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.setup(width= 800, height=600)
-screen.tracer(0)
+
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
-
 ball = Ball()
+
+
+screen.tracer(0)
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+r_paddle.speed("fastest")
+l_paddle.speed("fastest")   
 
 game_is_on =True
 while game_is_on:
-    
+    time.sleep(0.1)
     screen.update()
-    time.sleep(.2)
     ball.move()
 
     # collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # collision with r_paddle
+    # collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    # ball right_paddle out of bounds
+    if ball.xcor() > 380:
+        ball.reset_position()
+    
+    # detect left paddle out of bounds
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 screen.exitonclick()
