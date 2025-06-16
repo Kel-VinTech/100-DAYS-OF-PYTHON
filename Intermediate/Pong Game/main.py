1b59d2852514c11b609b907e14916588c8092dc0
kel-vintech <kelvinrexxy@gmail.com>
2025-06-16 11:22:43 +0100

    added score

--- Intermediate/Pong Game/main.py
+++ Intermediate/Pong Game/main.py
@@ -1,20 +1,20 @@
 from turtle import Turtle,Screen
 from pong_paddle import Paddle
 from pong_ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.setup(width= 800, height=600)
-
+screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball()
+Scoreboard = Scoreboard()
 
-
-screen.tracer(0)
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
