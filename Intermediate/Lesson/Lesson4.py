a30635ca992aa17821d9817745aeb65c9bc19dc9
kel-vintech <kelvinrexxy@gmail.com>
2025-06-20 01:56:59 +0100

    changes

---
+++ Intermediate/Lesson/Lesson4.py
@@ -0,0 +1,12 @@
+# open and read a file 
+
+# file = open("Intermediate/Lesson/my_file.txt", mode = "w")
+# content = file.read()
+# print(content)
+
+
+# with open("Intermediate/Lesson/my_file.txt", mode = "w") as file:
+
+#     file.write("Hello, still learning the file system in Python!")
+
+
