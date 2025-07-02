1be47c752cabc3fbf8706a80f9c0f55fa3c64867
kel-vintech <kelvinrexxy@gmail.com>
2025-07-02 08:39:37 +0100

    completed

---
+++ Intermediate/Rain Alert/main.py
@@ -0,0 +1,29 @@
+import requests
+api_key = "ac772c593742edde8cc6ea973f24fc30"
+
+
+"""Learning about Apis authentication"""
+
+weather_param = {
+    "lat": 9.5,
+    "lon": 2.25,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+
+
+API_LOCATION = "https://api.openweathermap.org/data/2.5/forecast"
+
+response = requests.get(API_LOCATION,params = weather_param)
+print(response.raise_for_status())
+data = response.json()
+weather_id = data["list"][0]["weather"][0]["id"]
+weather_desc = data["list"][0]["weather"][0]["description"]
+print(weather_id)
+print(weather_desc)
+
+for forcast in data["list"]:
+    if weather_id < 700:
+        print("bring an umbrella")
+
