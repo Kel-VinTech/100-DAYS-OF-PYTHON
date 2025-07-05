2838e5088d3b9aca1fda8f03ba7eb30c1da66127
kel-vintech <kelvinrexxy@gmail.com>
2025-07-05 23:56:33 +0100

    added new code

--- Intermediate/Habit_Tracker/main.py
+++ Intermediate/Habit_Tracker/main.py
@@ -1,1 +1,44 @@
-""" Advance post request"""
+import requests
+
+""" Advance HTTP request
+- GET - get data from an external service
+- POST - give an external service a data without worrying aout the response
+- PUT - update an external data
+- DELETE - delete a piece of data from the external service
+"""
+
+#HTTP REQUEST 
+
+USERNAME = "david404"
+TOKEN = "twoef12573rwef"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_param = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+
+
+# response = requests.post(url = pixela_endpoint, json = user_param)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_param = {
+    "id": "kaddy2",
+    "name": "cycling graph",
+    "unit": "km",
+    "type": "float",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+response = requests.post(url = graph_endpoint, json = graph_param, headers= headers)
+print(response.text)
+
