71c8112142d020a27b27a6f3af4bf0f12b552f80
kel-vintech <kelvinrexxy@gmail.com>
2025-07-15 18:29:13 +0100

    done

--- Intermediate/Selenuim_web/Interaction.py
+++ Intermediate/Selenuim_web/Interaction.py
@@ -5,6 +5,10 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
+
+
+
 
 # Setup Chrome
 options = webdriver.ChromeOptions()
@@ -12,29 +16,38 @@ options.add_argument("--window-size=1920,1080")  # Ensure desktop view
 # options.add_argument("--headless")  # Uncomment if you want headless
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
+
 # Open the page
-driver.get("https://secure-retreat-92358.herokuapp.com/")
+# driver.get("https://secure-retreat-92358.herokuapp.com/")
 
-# Wait for search box to be present first
-# wait = WebDriverWait(driver, 10)
-# fname = wait.until(EC.presence_of_element_located((By.NAME, "fName")))
-# lName = wait.until(EC.presence_of_all_elements_located((By.NAME, "lName")))
-# wait.until(EC.element_to_be_clickable((By.NAME, "search")))
+# # Wait for search box to be present first
+# # wait = WebDriverWait(driver, 10)
+# # fname = wait.until(EC.presence_of_element_located((By.NAME, "fName")))
+# # lName = wait.until(EC.presence_of_all_elements_located((By.NAME, "lName")))
+# # wait.until(EC.element_to_be_clickable((By.NAME, "search")))
 
-fname = driver.find_element(By.NAME, "fName")
-lname = driver.find_element(By.NAME, "lName")
-email = driver.find_element(By.NAME, "email")
-button = driver.find_element(By.CLASS_NAME, "btn")
+# fname = driver.find_element(By.NAME, "fName")
+# lname = driver.find_element(By.NAME, "lName")
+# email = driver.find_element(By.NAME, "email")
+# button = driver.find_element(By.CLASS_NAME, "btn")
 
-# Type "python" and press Enter
-fname.send_keys("Kelvin")
-lname.send_keys("Leven")
-email.send_keys("kingdavidxr@gmail.com")
-button.click()
-# Quit the driver
+# # Type "python" and press Enter
+# fname.send_keys("Kelvin")
+# lname.send_keys("Leven")
+# email.send_keys("kingdavidxr@gmail.com")
+# button.click()
 
-import time
-time.sleep(3)
+# # Quit the driver
+# import time
+# time.sleep(3)
+# driver.quit()
+
+url = "https://ozh.github.io/cookieclicker/"
+driver.get(url)
+select = Select(driver.find_element(By.ID, "langSelect"))
+select.select_by_value("EN")
+
+
+
 driver.quit()
-
 
