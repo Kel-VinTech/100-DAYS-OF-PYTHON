554b215df3da9a6393b259f0ee6b88f2fc9ef73a
kel-vintech <kelvinrexxy@gmail.com>
2025-07-16 23:53:55 +0100

    updated

--- Intermediate/Automated_Job_bot/main.py
+++ Intermediate/Automated_Job_bot/main.py
@@ -2,6 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
 
 
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+url = "https://www.linkedin.com/jobs/search/?currentJobId=4268101048&f_AL=true&f_E=1%2C2&f_WT=2&geoId=103644278&keywords=social%20media%20manager&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true&sortBy=DD"
+driver.get(url=url)
+
+
+time.sleep(3)
+driver.quit()
 
